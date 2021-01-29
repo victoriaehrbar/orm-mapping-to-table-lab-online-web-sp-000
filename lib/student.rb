76b3a77f39d4f0208b6aa819eb004bfb52144063
victoriaehrbar <victoriaehrbar@gmail.com>
2021-01-29 23:32:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,11 @@
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end 
   
 end
